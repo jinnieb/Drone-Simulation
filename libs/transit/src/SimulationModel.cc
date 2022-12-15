a5d0fa7f45e5bb636eeab42ab0a291fdaaf088aa
Enkhjin <boldb002@umn.edu>
2022-12-15 13:06:26 -0600

    Fixed popup for repair

--- libs/transit/src/SimulationModel.cc
+++ libs/transit/src/SimulationModel.cc
@@ -27,7 +27,7 @@ void SimulationModel::CreateEntity(JsonObject& entity) {
   
   if (type.compare("repair") == 0) {
     repairStations.push_back(myNewEntity);
-    controller.SendEventToView("RepairStationCreated", entity);S
+    controller.SendEventToView("RepairStationCreated", entity);
   }
   if (type.compare("drone") == 0) {
     myNewEntity = new DurabilityDecorator(myNewEntity, repairStations);
